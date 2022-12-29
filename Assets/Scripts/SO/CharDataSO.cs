f353dc888e2bfffc07c4c348e1c462c0dce547f2
gondr99 <gondr99@gmail.com>
2022-12-29 09:49:23 +0900

    init

---
+++ Assets/Scripts/SO/CharDataSO.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "SO/CharDataSO")]
+public class CharDataSO : ScriptableObject
+{
+    public int maxHP;
+    public int dex;
+    public float critical;
+}
